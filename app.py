62fc60c0fa78b8cdc1664f9cf97992888309eb99
João Victor Macedo <joaovictormacedo@outlook.com>
2023-10-07 18:30:21 -0300

    Desafio: Custom Message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "Hello World"
+    return "Olá, essa é minha aplicação em Python e Flask"
 
 if __name__ == '__main__':
     app.run()
